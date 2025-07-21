9d7cd8242e55d0624db718e4d3f70f9f4f25774c
smuddlee <sunart35up@gmail.com>
2025-07-21 10:42:57 +0700

    Update logger.py

--- AyiinXd/clients/logger.py
+++ AyiinXd/clients/logger.py
@@ -17,7 +17,7 @@ from AyiinXd import bot, branch, tgbot
 from AyiinXd.ayiin import ayiin_version as py_ver
 from AyiinXd.ayiin import HOSTED_ON, checking
 
-MSG_ON = "𝗔𝗰𝘁𝗶𝘃𝗲.\n     Xall 𝘷𝘦𝘳 - {} •[{}]•\n     Userbot 𝘷𝘦𝘳 - {}\n     Ketik `.ping` untuk cek ubot."
+MSG_ON = "𝗔𝗰𝘁𝗶𝘃𝗲.\n     muddle 𝘷𝘦𝘳 - {} •[{}]•\n     Userbot 𝘷𝘦𝘳 - {}\n     Ketik `.ping` untuk cek ubot."
 
 
 async def ayiin_userbot_on():
