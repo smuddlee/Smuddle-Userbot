fde9a88f774608089431399368d7a82486de34f7
smuddlee <sunart35up@gmail.com>
2025-07-21 10:34:01 +0700

    Update __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -29,7 +29,7 @@ try:
     LOGS.info(f"Telethon Version - {version.__version__} [Layer: {LAYER}]")
     LOGS.info(f"PyTgCalls Version - {pytgcalls}")
     LOGS.info(f"Userbot Version - {ubotversion} •[{adB.name}]•")
-    LOGS.info(f"IXALL Version - {ayiin_version} •[{HOSTED_ON}]•")
+    LOGS.info(f"Smuddle Version - {ayiin_version} •[{HOSTED_ON}]•")
     LOGS.info("[🔥 BERHASIL DIAKTIFKAN! 🔥]")
 except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
     pass
