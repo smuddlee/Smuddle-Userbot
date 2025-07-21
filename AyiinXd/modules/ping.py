4f0d236bcf80482819c10bb64ead1832a26a9ae9
smuddlee <sunart35up@gmail.com>
2025-07-21 10:51:13 +0700

    Update ping.py

--- AyiinXd/modules/ping.py
+++ AyiinXd/modules/ping.py
@@ -54,10 +54,9 @@ async def _(ping):
     duration = (end - start).microseconds / 1000
     user = await ping.client.get_me()
     sleep(3)
-    await ping.reply(
-        f"私 **ᴘɪɴɢ :** `%sms`\n"
-        f"さ **ᴜᴘᴛɪᴍᴇ :** `{uptime}` \n"
-        f"ふ **ᴏᴡɴᴇʀ :** [{user.first_name}](tg://user?id={user.id})" % (duration)
+    await ping.edit(
+        f"**ᴘɪɴɢ :** `%sms`\n"
+        f"**ᴜᴘᴛɪᴍᴇ :** `{uptime}` \n"
     )
 
 
@@ -109,14 +108,14 @@ async def _(pong):
         f" 𖤐 𝗛𝗔𝗩𝗘 𝗬𝗢𝗨𝗥 𝗣𝗜𝗡𝗚 "
         f"\n ★ 𝗬𝗼𝘂𝗿 𝗣𝗶𝗻𝗴 `%sms` \n"
         f"★ 𝗨𝗽𝘁𝗶𝗺𝗲 "
-        f"\n ★ User - 𝗜𝗫𝗔𝗟𝗟『[{user.first_name}](tg://user?id={user.id})』 \n" % (duration)
+        f"\n ★ User - smuddle『[{user.first_name}](tg://user?id={user.id})』 \n" % (duration)
     )
 
 
 # .keping & kping Coded by Koala
 
 
-@ayiin_cmd(pattern=r"xall$")
+@ayiin_cmd(pattern=r"mudle$")
 async def _(pong):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
@@ -138,14 +137,14 @@ async def _(pong):
     await kping.edit("8===🥹D")
     await kping.edit("8===😬D🔥")
     await kping.edit("8====D🔥🔥")
-    await kping.edit("**IXALLL**")
-    await kping.edit("**DISINI IXALL GANTENG KECE BADAI SLEBEW .....**")
+    await kping.edit("**SMUDDLE**")
+    await kping.edit("**DISINI 𝗦𝗺𝘂𝗱𝗱𝗹𝗲-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 KECE BADAI SLEBEW .....**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await kping.edit("☠️")
     sleep(3)
     await kping.edit(
-        f"**IXALL TAMVAN!! 💀**\n𝗬𝗼𝘂𝗿 𝗣𝗶𝗻𝗴 : %sms\n𝗨𝗽𝘁𝗶𝗺𝗲 : {uptime}🕛" % (duration)
+        f"**𝗦𝗺𝘂𝗱𝗱𝗹𝗲-𝗨𝘀𝗲𝗿𝗯𝗼𝘁!! 💀**\n𝗬𝗼𝘂𝗿 𝗣𝗶𝗻𝗴 : %sms\n𝗨𝗽𝘁𝗶𝗺𝗲 : {uptime}🕛" % (duration)
     )
 
 
@@ -195,7 +194,7 @@ async def _(pong):
 CMD_HELP.update(
     {
         "ping": f"**Plugin : **`ping`\
-        \n\n  »  **Perintah :** `{cmd}ping` ; `{cmd}lping` ; `{cmd}xping` ; `{cmd}xall`\
+        \n\n  »  **Perintah :** `{cmd}ping` ; `{cmd}lping` ; `{cmd}xping` ; `{cmd}mudle`\
         \n  »  **Kegunaan : **Untuk menunjukkan ping userbot.\
         \n\n  »  **Perintah :** `{cmd}pong`\
         \n  »  **Kegunaan : **Sama seperti perintah ping\
