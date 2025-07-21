a1b7d7073d8e325ae9eb5dd78e1ff1d0d6514498
smuddlee <sunart35up@gmail.com>
2025-07-21 10:46:15 +0700

    Update events.py

--- AyiinXd/ayiin/events.py
+++ AyiinXd/ayiin/events.py
@@ -90,26 +90,26 @@ async def get_user_from_event(
 
 async def ajg():
     try:
-        await bot(Get("jasebxall"))
-        await bot(Get("ixallsupport"))
-        await bot(Get("proofxall"))
-        await bot(Get("nerpxus"))
+        await bot(Get("smuddle"))
+        #await bot(Get("ixallsupport"))
+        #await bot(Get("proofxall"))
+        #await bot(Get("nerpxus"))
     except rpcerrorlist.ChannelPrivateError:
         print("error, need unbanned!")
         sys.exit(1)
 
 
 async def checking(client):
-    gocheck = str(pybase64.b64decode("QGphc2VieGFsbA=="))[2:13]
-    checker = str(pybase64.b64decode("QGl4YWxsc3VwcG9ydA="))[2:17]
-    checkxd = str(pybase64.b64decode("QHByb29meGFsbA=="))[2:13]
-    checksbt = str(pybase64.b64decode("QG5lcnB4dXM="))[2:14]
+    gocheck = str(pybase64.b64decode("QHNtdWRkbGU="))[2:13]
+    #checker = str(pybase64.b64decode("QGl4YWxsc3VwcG9ydA="))[2:17]
+    #checkxd = str(pybase64.b64decode("QHByb29meGFsbA=="))[2:13]
+    #checksbt = str(pybase64.b64decode("QG5lcnB4dXM="))[2:14]
     if client:
         try:
             await client(Get(gocheck))
-            await client(Get(checker))
-            await client(Get(checkxd))
-            await client(Get(checksbt))
+            #await client(Get(checker))
+            #await client(Get(checkxd))
+            #await client(Get(checksbt))
         except rpcerrorlist.ChannelPrivateError:
             print("error, need unbanned!")
             sys.exit(1)
