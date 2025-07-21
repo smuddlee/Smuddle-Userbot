14c2353021560704c6c7e43df80442c2309d5fba
smuddlee <sunart35up@gmail.com>
2025-07-21 10:48:36 +0700

    Update system_stats.py

--- AyiinXd/modules/system_stats.py
+++ AyiinXd/modules/system_stats.py
@@ -183,7 +183,7 @@ async def amireallyalive(alive):
     await alive.edit("⚡️")
     await asyncio.sleep(3)
     output = (
-        f"**here is [𝗜𝗫𝗔𝗟𝗟-Userbot](t.me/jasebxall)**\n\n"
+        f"**here is [𝗦𝗺𝘂𝗱𝗱𝗹𝗲-𝗨𝘀𝗲𝗿𝗯𝗼𝘁](t.me/smuddle)**\n\n"
         f"**{alive_text}**\n\n"
         f"━━━━━━━━━━━━━━\n"
         f"** - Bᴀsᴇ Oɴ :** ••{adB.name}••\n"
@@ -193,8 +193,8 @@ async def amireallyalive(alive):
         f"** - Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{python_version()}` \n"
         f"** - PʏTɢCᴀʟʟs Vᴇʀsɪᴏɴ :** `{pytgcallss}` \n"
         f"** - Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{version.__version__}` \n"
-        f"** - Pʏ-ʙʟᴜᴇ ᴠᴇʀsɪᴏɴ :** `{py_ver}`\n"
-        f"** - ɪxᴀʟʟ ᴠᴇʀsɪᴏɴ :** `{ayiin_version}` [{HOSTED_ON}]\n"
+        f"** - Pʏ-xall ᴠᴇʀsɪᴏɴ :** `{py_ver}`\n"
+        f"** - smuddle ᴠᴇʀsɪᴏɴ :** `{ayiin_version}` [{HOSTED_ON}]\n"
         f"** - Bᴏᴛ Uᴘᴛɪᴍᴇ :** `{uptime}`\n"
         "━━━━━━━━━━━━━━━\n\n"
     )
